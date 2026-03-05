0ed42282db362554ef249870d3e251f9a43ba55e
AkbarAj09 <210210502010@student.unm.ac.id>
2026-03-05 14:45:12 +0800

    update log

--- app/Http/Controllers/AmLevelUpController.php
+++ app/Http/Controllers/AmLevelUpController.php
@@ -691,7 +691,6 @@ class AmLevelUpController extends Controller
                     $poinSisaBulanLalu = $previousMonthPoints[$email] ?? 0;
                     
                     $totalpackagePoint = $packagePoint[$email] ?? 0;
-                    \Log::info("Email: {$email}, Package Point: " . ($packagePoint[$email] ?? 0));
                     if ($totalSettlement == 0 && $poinSisaBulanLalu == 0) {
                         continue;
                     }
