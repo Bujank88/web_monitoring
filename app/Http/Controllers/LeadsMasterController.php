74dbde99396334c684b4bcc746ec479c68282e31
vinsentiusalbert <37323233+vinsentiusalbert@users.noreply.github.com>
2026-08-20 23:54:27 +0700

    tambah filter

--- app/Http/Controllers/LeadsMasterController.php
+++ app/Http/Controllers/LeadsMasterController.php
@@ -40,6 +40,14 @@ class LeadsMasterController extends Controller
                     ->orderBy('flag_event')
                     ->pluck('flag_event')
             ),
+            'dataTypes' => Cache::remember('data_types_list_leads', 3600, fn() =>
+                DB::table('detail_leads_summary')
+                    ->whereNotNull('data_type')
+                    ->where('data_type', '!=', '')
+                    ->distinct()
+                    ->orderBy('data_type')
+                    ->pluck('data_type')
+            ),
         ]);
     }
 
