a9349612c7f899d7f29d138430b8afa6ddbb2f9a
albert <vinsentiusalbert@gmail.com>
2026-09-22 17:02:00 +0700

    filter area canvasser

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1288,7 +1288,7 @@
         // NOTE: Chart akan di-load setelah DataTable selesai (lazy loading)
 
         const locationsByArea = {
-            '1': ['Medan', 'Padang', 'Pekanbaru', 'Palembang', 'Jambi'],
+            '1': ['Medan', 'Padang', 'Pekanbaru', 'Palembang', 'Jambi', 'Batam'],
             '2': ['Jakarta', 'Bogor', 'Bandung', 'Depok', 'Tangerang', 'Bekasi'],
             '3': ['Semarang', 'Surabaya', 'Yogyakarta', 'Denpasar'],
             '4': ['Makassar', 'Manado', 'Samarinda', 'Banjarmasin']
